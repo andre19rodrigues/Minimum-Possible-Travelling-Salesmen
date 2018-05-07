e21c9c0a7895c08cbf0b99219bcbed71eb8aca9d
Unknown <fabio.pereira@ubi.pt>
2018-05-07 12:27:13 +0100

    Fix generate locations

--- GenerateRandomLocations.py
+++ GenerateRandomLocations.py
@@ -2,19 +2,19 @@ import numpy
 
 
 f = open('locations.csv', 'w+')
-total = 50
+total = 500
 
 for i in range(0, total):
     for j in range(0, total):
-        if (j is not total-1):
-            if (i is not j):
+        if (j != total-1):
+            if (i != j):
                 f.write(str(numpy.random.randint(1, 20))+',')
             else:
                 f.write(str(0)+',')
         else:
-            if (i is not j):
+            if (i != j):
                 f.write(str(numpy.random.randint(1, 20)))
             else:
                 f.write(str(0))
-    if i is not total-1:
+    if i != total-1:
         f.write('\n')
